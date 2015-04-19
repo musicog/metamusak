90cfcc794ccd035358bf9d53f54c59aefdfd0de4
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2015-04-19 12:32:08 +0100

    Refactor; now does full pipeline for annotator videos, including generation of sidecarts

--- metamusak.py
+++ metamusak.py
@@ -5,86 +5,87 @@ import pprint
 import csv
 import re
 import os
-# path to everything
-ringcycle = "/home/davidw/RingCycle/"
-# path to rdf folder
-rdfdir = ringcycle + "metamusak/"
-videos = list()
-# graph annotation page turns
-caroturns = list()
-caroturnreader = csv.reader( open (ringcycle + "data/carolin_pageturns_final.csv", "r"), delimiter=",", quotechar='"')
-#5 items: filesize, date, time, page, annotated_by
-for line in caroturnreader: 
-    caroturns.append(
-        {
-            "filesize":     line[0],
-            "date":         line[1],
-            "time":         line[2],
-            "page":         line[3],
-            "annotated_by": line[4]
-        }
-    )
 
-# do annotation videos
-for opera in os.listdir(ringcycle + 'video'):
-    for video in os.listdir(ringcycle + 'video/' + opera):
-        if video[0] == ".": 
-            continue
-        if video.endswith("m4v"): #TODO VERIFY
-            continue 
-        #mediainfo = check_output(["mediainfo", ringcycle + 'video/' + opera + '/' + video])
-       # for line in mediainfo.splitlines():
-        mediainfo = MediaInfo.parse(ringcycle + 'video/' + opera + '/' + video)
-        thisfile = dict()
-        for track in mediainfo.tracks:
-            if track.track_type == "General":
-                thisfile["rdftool_opera"] = opera
-                thisfile["rdftool_filename"] = video
-                thisfile["rdftool_filepath"] = ringcycle + "video/" + opera + "/"
-                thisfile["duration"] = track.duration
-                thisfile["encoded_date"] = track.encoded_date
-                thisfile["file_size"] = track.file_size
-                thisfile["file_name"] = track.file_name
-                thisfile["format"] = track.format
-            if track.track_type == "Video":
-                thisfile["video_codec"] = track.codec
-                thisfile["video_codec_url"] = track.codec_url
-                thisfile["video_display_aspect_ratio"] = track.display_aspect_ratio
-                thisfile["video_format"] = track.format
-                thisfile["video_bit_depth"] = track.bit_depth
-                thisfile["video_bit_rate"] = track.bit_rate
-                thisfile["video_sampling_rate"] = track.sampling_rate
-                thisfile["video_resolution"] = track.resolution
-                thisfile["video_height"] = track.height
-                thisfile["video_width"] = track.width
-                thisfile["video_duration"] = track.duration
-            if track.track_type == "Audio":
-                thisfile["audio_bit_rate"] = track.bit_rate
-                thisfile["audio_codec"] = track.codec
-                thisfile["video_duration"] = track.duration
-        videos.append(thisfile)
+g = Graph() # The Graph Dracula
 
-#for v in videos:
-#    for k in v:
-#        print k, ": ", v[k]
+def generateSidecarts(performances, templates, filebase, rdfbase):
+    # for each performance -- replace this with list of ids from magic spreadsheet 
+    for performance in performances:
+        # annotator videos
+        annotatordir = filebase + "{0}/annotator/".format(performance)
+        annotatorrdf = rdfbase + "{0}/annotator/".format(performance)
+        for video in os.listdir(annotatordir):
+            videobase = os.path.splitext(video)[0]
+            if video[0] == ".": #skip hidden files
+                continue
+            mediainfo = getMediaInfo(annotatordir + video)
+            for key in mediainfo:
+                if mediainfo[key] is None: 
+                    continue # skip blank values
+                g.parse(data='<{uri}> <{key}> "{value}" .'.format(uri = annotatorrdf + videobase, key = key, value = mediainfo[key]), format="turtle")
+            sidecart = open(annotatordir + videobase + ".ttl", 'w')
+            constructQ = templates["annotator"].format(uri = annotatorrdf + videobase, performance = rdfbase + performance)
+            rdfresult = g.query(constructQ)
+            sidecart.write(rdfresult.serialize(format="turtle"))
+            #TODO Add some nice exception catching here
+            print "Wrote to {0}".format(annotatordir + videobase + ".ttl")
+            sidecart.close()
+
+def getMediaInfo(mediaFile):
+    mediainfo = MediaInfo.parse(mediaFile)
+    thisfile = dict()
+    for track in mediainfo.tracks:
+        if track.track_type == "General":
+#                thisfile["rdftool_opera"] = opera
+#                thisfile["rdftool_filename"] = video
+#                thisfile["rdftool_filepath"] = ringcycle + "video/" + opera + "/"
+            thisfile["http://www.w3.org/ns/ma-ont#duration"] = track.duration
+            thisfile["http://www.w3.org/ns/ma-ont#date"] = track.encoded_date
+#                thisfile["file_size"] = track.file_size
+            thisfile["http://www.w3.org/ns/ma-ont#title"] = track.file_name
+            thisfile["http://www.w3.org/ns/ma-ont#format"] = track.format
+            thisfile["http://www.w3.org/ns/ma-ont#averageBitRate"] = track.overall_bit_rate
+        if track.track_type == "Video":
+#                thisfile["video_codec"] = track.codec
+#                thisfile["video_codec_url"] = track.codec_url
+#                thisfile["video_display_aspect_ratio"] = track.display_aspect_ratio
+            thisfile["http://www.w3.org/ns/ma-ont#format"] = track.format
+#                thisfile["video_bit_depth"] = track.bit_depth
+#                thisfile["video_bit_rate"] = track.bit_rate
+            thisfile["http://www.w3.org/ns/ma-ont#frameRate"] = track.frame_rate
+            thisfile["http://www.w3.org/ns/ma-ont#frameSize"] = str(track.width) + 'x' + str(track.height)
+#                thisfile["video_resolution"] = track.resolution
+#                thisfile["video_height"] = track.height
+#                thisfile["video_width"] = track.width
+#                thisfile["video_duration"] = track.duration
+        if track.track_type == "Audio":
+#                thisfile["audio_bit_rate"] = track.bit_rate
+#                thisfile["audio_codec"] = track.codec
+#                thisfile["video_duration"] = track.duration
+            thisfile["http://www.w3.org/ns/ma-ont#format"] = track.format
+    return thisfile
 
 
-# TIMELINE LOGIC
-# Annotation page turn timeline:
-# We need to bootstrap durations
-# Do this by:
-#   - Figure out which pages go with which opera
-#   - For each opera
-#       - For each page
-#           - Add a "duration" which is the timestamp of the NEXT page minus the timestamp of THIS page
-#           - If there is no next page, make the duration a constant?
+if __name__ == "__main__":
+    templates = { # TODO read these from an external directory? perhaps?
+        "annotator": #annotator video
+            """
+            PREFIX dct: <http://purl.org/dc/terms/>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            CONSTRUCT {{ 
+                <{uri}> rdfs:comment "TODO More stuff on how this fits in to the performance / series" ;
+                      dct:isPartOf <{performance}> .
+                <{uri}> ?p ?o .
+            }} WHERE {{ 
+                <{uri}> ?p ?o .
+            }}
+            """
+    }
 
-
-# Build the RDF graph
-g = Graph()
-for v in videos:
-    for k in v:
-        g.parse(data='<http://example.com/{filename}> <http://example.com/{key}> "{value}" .'.format(filename = v["file_name"], key = k, value = v[k]), format="turtle")
-
-result = g.query('CONSTRUCT { ?track ?p ?o } WHERE { BIND(<http://example.com/BDMV> as ?track) . ?track ?p ?o }')
-print result.serialize(format="turtle")
+    # physical path
+    ringcycle = "/home/davidw/MetaRingCycle/performance/"
+    performances = os.listdir(ringcycle)
+    # rdf path
+    rdfbase = "http://performance.data.t-mus.org/performance/" 
+    generateSidecarts(performances, templates, ringcycle, rdfbase)
+    print "Done!"
